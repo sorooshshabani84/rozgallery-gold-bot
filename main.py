cb5163374c5232cc4ff14a77d4e03ac8226e116c
sorooshshabani84 <shabanisoroosh7@gmail.com>
2026-06-15 01:16:57 +0330

    Update main.py

--- main.py
+++ main.py
@@ -1,39 +1,18 @@
 import os
-import time
-import schedule
 import requests
 
-BOT_TOKEN = os.getenv("8682876117:AAHcJxShwgr0UufSW5QntUWaSWwea1PQGjY")
+BOT_TOKEN = os.getenv("BOT_TOKEN")
 CHANNEL = "@rozgallerydaily"
 
-def send_message(text):
-    url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
-    requests.post(url, json={
+url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
+
+r = requests.post(
+    url,
+    json={
         "chat_id": CHANNEL,
-        "text": text
-    })
+        "text": "✅ تست ربات Railway"
+    }
+)
 
-def job():
-    text = """📈 بازار طلا و ارز
-
-🟡 طلای ۱۸ عیار: در حال دریافت...
-💵 دلار: در حال دریافت...
-🌍 اونس جهانی: در حال دریافت...
-
-🪙 سکه امامی: در حال دریافت...
-🪙 نیم سکه: در حال دریافت...
-🪙 ربع سکه: در حال دریافت...
-
-@rozgallerydaily
-"""
-    send_message(text)
-
-# اجرای اولیه
-job()
-
-# هر 10 دقیقه
-schedule.every(10).minutes.do(job)
-
-while True:
-    schedule.run_pending()
-    time.sleep(30)
+print(r.status_code)
+print(r.text)
